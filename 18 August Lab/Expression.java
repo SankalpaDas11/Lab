8e9b63ffd0f514462b157048ac1d900c4470fdfa
Sankalpa Das <d2sankalpa@gmail.com>
2022-08-18 17:41:29 +0530

    Add files via upload

---
+++ 18 August Lab/Expression.java
@@ -0,0 +1,11 @@
+// Q.1 Write a Java program to find the value of specified expression.
+public class Expression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double sol = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5); // logic - storing the answer in a variable
+		System.out.println("The answer of the given expression is (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)-> " + sol); // printing the result of the given expression
+
+	}
+
+}
