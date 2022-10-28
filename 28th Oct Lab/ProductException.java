d90e711d416ba32116e6370da78731127342f0e1
Sankalpa Das <d2sankalpa@gmail.com>
2022-10-28 17:15:42 +0530

    Update ProductException.java

--- 28th Oct Lab/ProductException.java
+++ 28th Oct Lab/ProductException.java
@@ -4,33 +4,35 @@
 
 package Exception;
 import java.util.Scanner;
-class InvailException extends Exception { //sub class of exception
+class InvailException extends Exception { 								//sub class of exception
 	public InvailException(String str) {
 		super(str);
 	}
 }
 class Product{ // class with power method
-	public int product(int x, int y) throws InvailException{ //method power to calculate result
-		if(x<0 && y<0) { // checking if any of the inputs is negative
+	public int product(int x, int y) throws InvailException{ 					//method product to calculate result
+		if(x<0 && y<0) { 									// checking if any of the inputs is negative
 			throw new 
 			InvailException("java.lang.Exception: x and y both should not be negative");}
-		else if (x==0 || y==0) { // checking if any of the inputs is 0
+		else if (x==0 || y==0) { 								// checking if any of the inputs is 0
 			throw new 
 			InvailException("java.lang.Exception: x and y"
 					+ " should not be 0");}
-		else { // calculating result
-		return x*y; // logic to return a long type result with n^p
+		else { 											// calculating result
+		return x*y; 										// logic to return the product
 		}
 	}
 }
-public class ProductException { // class with main method
-	public static void main(String[] args) throws InvailException { //main method
-		Product p = new Product(); // creating object of the previous class
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter two integers to get the product : ");
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		//calling power method with different inputs
+public class ProductException { 									// class with main method
+	public static void main(String[] args) throws InvailException {					//main method
+		Product p = new Product(); 								// creating object of the previous class
+		Scanner sc = new Scanner(System.in); 							//scanner class
+		System.out.println("Enter two integers to get the product : "); 			//print line to ask user for inputs
+		int x = sc.nextInt(); //storing the 1st integer input
+		int y = sc.nextInt(); //storing the 2nd integer input
+		
+		
+		//calling power method with different user inputs
 		{try{System.out.println(p.product(x,y));}  
 		catch(InvailException u) {
 			System.out.println(u.getMessage());}}	
