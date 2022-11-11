051a87fffdf13be0751b88175624fd7337d06bf1
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-11 13:47:53 +0530

    Update LineArray.java

--- 10th Nov Lab/LineArray.java
+++ 10th Nov Lab/LineArray.java
@@ -1,4 +1,5 @@
 //Q.3 WAJP to store text file content line by line into an array.
+
 package IO;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -7,25 +8,31 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+
 public class LineArray {
+    
     public static void main(String a[]){
-        StringBuilder sb = new StringBuilder(); // creating object of string builder
-        String strLine = ""; // string type variable to store the lines
-        List<String> list = new ArrayList<String>(); // taking a new string type arraylist
+        
+        StringBuilder sb = new StringBuilder();             // creating object of string builder
+        String strLine = "";                                // string type variable to store the lines
+        List<String> list = new ArrayList<String>();        // taking a new string type arraylist
+        
         try {
              BufferedReader br = new BufferedReader(new FileReader("F://test.txt")); // input file path in bufferedbuilder object
               while (strLine != null)
                {
-                strLine = br.readLine(); // storing lines
-                sb.append(strLine); // adding lines
+                strLine = br.readLine();                        // storing lines
+                sb.append(strLine);                             // adding lines
                 sb.append(System.lineSeparator());
                 strLine = br.readLine();
-                if (strLine==null) // checking whether any lines left
+                if (strLine==null)                              // checking whether any lines left
                    break;
                 list.add(strLine);
             }
-         System.out.println(Arrays.toString(list.toArray())); // printing array 
+            
+         System.out.println(Arrays.toString(list.toArray()));   // printing array 
              br.close();
+            
         } catch (FileNotFoundException e) {
             System.err.println("File not found");
         } catch (IOException e) {
