8b9c643c37110499da23b63c857b1390aa4a945f
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-17 18:13:26 +0530

    Update InsertFirst.java

--- 17th Nov Lab/InsertFirst.java
+++ 17th Nov Lab/InsertFirst.java
@@ -1,12 +1,12 @@
 // Q.5 WAJP to insert the specified element at the front of a linked list
 package LAB17Nov;
 
-import java.util.LinkedList; // importing lined list
+import java.util.LinkedList; // importing linked list
 
 public class InsertFirst {
 
 	public static void main(String[] args) {
-LinkedList<String> ll = new LinkedList<String>(); // creating new linkedlist named 'll'
+LinkedList<String> ll = new LinkedList<String>(); 		// creating new linkedlist named 'll'
 		
 		// adding elements in ll by add() method
 		ll.add("India");
@@ -17,7 +17,7 @@ LinkedList<String> ll = new LinkedList<String>(); // creating new linkedlist nam
 		
 		System.out.println("Before inserting : " + ll); // printing original linkedlist
 		
-		ll.addFirst("USA"); // adding element in the 1st position
-		System.out.println("After inserting : " + ll); // printing final linkedlist
+		ll.addFirst("USA"); 				// adding element in the 1st position
+		System.out.println("After inserting : " + ll); 	// printing final linkedlist
 	}
 }
