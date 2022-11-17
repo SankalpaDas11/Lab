e5818f430dd3fe8bf8ac4766ba686179e6f730f2
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-17 18:12:08 +0530

    Update ExtractPortion.java

--- 17th Nov Lab/ExtractPortion.java
+++ 17th Nov Lab/ExtractPortion.java
@@ -1,13 +1,13 @@
 // Q.3 WAJP to extract a portion of an array list
 package LAB17Nov;
 
-import java.util.ArrayList; // importing ArrayList
-import java.util.List; // List
+import java.util.ArrayList; 	// importing ArrayList
+import java.util.List; 		// importing List
 
 public class ExtractPortion {
 
 	public static void main(String[] args) {
-		List<String> fll = new ArrayList<String>(); // creating new arraylist named 'fll'
+		List<String> fll = new ArrayList<String>(); 	// creating new arraylist named 'fll'
 		
 		// adding elements in fll by add() method
 		fll.add("India");
@@ -15,10 +15,10 @@ public class ExtractPortion {
 		fll.add("Russia");
 		fll.add("Iran");
 		fll.add("China");
-		System.out.println("1st array list : " + fll); // printing 1st array list
+		System.out.println("1st array list : " + fll); 	// printing 1st array list
 
-		List<String> sll = new ArrayList<String>(); // creating new arraylist named 'sll'
-		sll = fll.subList(0, 3); // extracting 1st position to 3rd position
-		System.out.println("2nd array list : " + sll); // printing sdcond arraylist
+		List<String> sll = new ArrayList<String>(); 	// creating new arraylist named 'sll'
+		sll = fll.subList(0, 3); 			// extracting 1st position to 3rd position
+		System.out.println("2nd array list : " + sll);	// printing second arraylist
 	}
 }
