89359187031ec83b690ef22aedacce6482c59943
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-17 18:09:17 +0530

    Update CompareArrayLists.java

--- 17th Nov Lab/CompareArrayLists.java
+++ 17th Nov Lab/CompareArrayLists.java
@@ -1,13 +1,15 @@
 //Q.4 WAJP to compare two array lists
+
 package LAB17Nov;
-import java.util.ArrayList; // importing ArrayList
-import java.util.List; // importing List
+
+import java.util.ArrayList; 	// importing ArrayList
+import java.util.List; 		// importing List
+
 public class CompareArrayLists {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		List<String> fll = new ArrayList<String>(); // creating new arraylist named 'fll'
+		List<String> fll = new ArrayList<String>();		// creating new arraylist named 'fll'
 		
 		// adding elements in fll by add() method
 		fll.add("India");
@@ -15,19 +17,19 @@ public class CompareArrayLists {
 		fll.add("Russia");
 		fll.add("Iran");
 		fll.add("China");
-		System.out.println("1st array list : " + fll); // printing 1st array list
+		System.out.println("1st array list : " + fll); 		// printing 1st array list
 		
-		List<String> sll = new ArrayList<String>(); // creating new arraylist named 'sll'
+		List<String> sll = new ArrayList<String>(); 		// creating new arraylist named 'sll'
 		
 		// adding elements in sll by add() method
 		sll.add("India");
 		sll.add("Japan");
 		sll.add("Russia");
 		sll.add("China");
-		System.out.println("2nd array list : " + sll); // printing 2nd array list
+		System.out.println("2nd array list : " + sll); 		// printing 2nd array list
 		
 		for(String i : fll) {
-			if(sll.contains(i)) // checking if the element is present or not
+			if(sll.contains(i)) 				// checking if the element is present or not
 				System.out.println("Yes " + i + " is present in both the arraylist.");
 			else
 				System.out.println("No " + i + " is not present in the 2nd arraylist.");
