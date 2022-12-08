db7da4441799a8b0a6f969d8ce914ee2fe7935e6
Sankalpa Das <d2sankalpa@gmail.com>
2022-12-08 16:22:58 +0530

    Update BookUtil.java

--- 17th Nov Lab/BookUtil.java
+++ 17th Nov Lab/BookUtil.java
@@ -1,30 +1,32 @@
 package com.bookutil;
 
-import java.util.Scanner;
+import java.util.Scanner;  //Scanner class 
 
-import com.book.Book;
+import com.book.Book; 	// importing other to classes to access the methods
 import com.book.BookStore;
 
 public class BookUtil {
 	public static void main(String ...args) {
-		// initializing BookStore class
+									// initializing BookStore class
 		BookStore bs = new BookStore();
-		// for loop to take user input to read data from the user for 3 book object
+									// for loop to take user input to read data from the user for 3 book object
 		
 		for(int i=0; i<3; i++) {
-			System.out.println("Enter BookId:- ");
-			Scanner addBookId = new Scanner(System.in);
-			String bookId = addBookId.nextLine();
-			System.out.println("Enter Title:- ");
+			System.out.println("Enter The BookId : "); 	// asking the book id from user
+			Scanner addBookId = new Scanner(System.in);	
+			String bookId = addBookId.nextLine();		// storing book id
+			
+			System.out.println("Enter The Title : ");	// asking the title from user
 			Scanner addtitle = new Scanner(System.in);
-			String title = addtitle.nextLine();
-			System.out.println("Enter Author Name:- ");
+			String title = addtitle.nextLine();		// storing title
+			
+			System.out.println("Enter Author Name : ");	// asking the author name
 			Scanner addAuthor = new Scanner(System.in);
- //		Scanner addSA = new Scanner(System.in);
-//		String SA = addSA.nextLine();
-//		bs.searchByAuthor(SA);
-		bs.searchByTitle("Java");
-//		bs.searchByAuthor("Mr.XYZ");
-//		bs.displayAll();
+ 			Scanner addSA = new Scanner(System.in);		// storing the author name
+		String SA = addSA.nextLine();
+		bs.searchByAuthor(SA);					// calling method to search book by author name
+		bs.searchByTitle("Java");				// calling method to search book by book title
+		bs.searchByAuthor("Mr.XYZ");				
+		bs.displayAll();					// showing all the details by calling method
 	}
 }}
