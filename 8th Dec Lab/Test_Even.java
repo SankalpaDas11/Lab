d423e06cdb353a7468a8deb830737e0feeda1f1b
Sankalpa Das <d2sankalpa@gmail.com>
2022-12-09 15:23:10 +0530

    Update Test_Even.java

--- 8th Dec Lab/Test_Even.java
+++ 8th Dec Lab/Test_Even.java
@@ -1,13 +1,13 @@
 package com.demo.test;
 
-import static org.junit.Assert.assertEquals; // importing assert
+import static org.junit.Assert.assertEquals; 	// importing assert
 
-import org.junit.Test; // importing test
+import org.junit.Test; 				// importing test
 
 import com.demo.main.EvenTrue;
 
 public class Test_Even {
-	@Test // Test annotation
+	@Test 						// Test annotation
 	public void test() {
 		assertEquals(true,EvenTrue.findEven()); // running test cases
 	}
